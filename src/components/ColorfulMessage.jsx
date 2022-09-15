dabb19719d491ac0f58292444b823e06619b91c0
Seiya830 <forest.sh90830@gmail.com>
2022-09-15 05:16:46 +0000

    ColorfulMessage.jsxを追加

---
+++ src/components/ColorfulMessage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const contentStyle = {
+        color: 'blue',
+        fontSize: '18px'
+    };
+
+const ColorfulMessage = () => {
+    return (
+        <p style={contentStyle}>こんばんは</p>
+    );
+};
+
+export default ColorfulMessage;
