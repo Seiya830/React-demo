b3bd7e922ee04218532a26d071c7e3c581a3c410
Seiya830 <forest.sh90830@gmail.com>
2022-09-15 02:41:32 +0000

    スタイルの変更

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import ReactDom from "react-dom";
 
 const App = () => {
     const onClickButton = () => alert();
+    const contentStyle = {
+        color: 'blue',
+        fontSize: '18px'
+    };
     return (
       <>
-        <h1>こんにちは！</h1>
-        <p>こんばんは</p>
+        <h1 style={{ color: 'red' }}>こんにちは！</h1>
+        <p style={contentStyle}>こんばんは</p>
         <button onClick={onClickButton}>ボタン</button>
        </>  
     );
